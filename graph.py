38d14663c7c355ac7b72a00fc16030629dbbda52
i2phi <jfiturbe@gmail.com>
2026-01-09 09:53:35 -0500

    refactor, workfolow as react agent

--- graph.py
+++ graph.py
@@ -1,10 +1,13 @@
+import operator
+
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
-from langchain.messages import SystemMessage, HumanMessage
-from langchain_core.messages import AnyMessage
-from typing_extensions import TypedDict, Annotated
-from langgraph.graph import StateGraph, START, END
-import operator
+from langchain.messages import HumanMessage, SystemMessage
+from langchain.tools import tool
+from langchain_core.messages import AIMessage, AnyMessage
+from langgraph.graph import END, START, StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
+from typing_extensions import Annotated, TypedDict
 
 _ = load_dotenv(override=True)
 
@@ -14,32 +17,57 @@ class MessagesState(TypedDict):
     llm_calls: int
 
 
+@tool
+def write_request_response(receiver: str, sender: str, content: str) -> str:
+    """
+    Write a request response message.
+    receiver: who the message is sent to
+    sender: who the message is sent from
+    content: the content of the message
+    """
+    return f"From {sender} to {receiver}: {content}"
+
+
 llm = init_chat_model("openai:gpt-5-nano")
+llm_with_tools = llm.bind_tools([write_request_response])
 
 
-def ingest_request(state: MessagesState) -> dict[str, list[AnyMessage] | int]:
-    """LLM node that calls the model and returns messages"""
-    messages: list[AnyMessage] = [
-        SystemMessage(content="Say hello in Spanish")
-    ] + state["messages"]
+def call_model(state: MessagesState) -> dict[str, list[AnyMessage] | int]:
+    messages = [
+        SystemMessage(
+            content=" You are a helpful assistant, if the user makes a direct request,write a brief response to the users request using the write_request_response tool then show the user the response"
+        )
+    ] + state.get("messages")
+    response = llm_with_tools.invoke(messages)
 
-    response: AnyMessage = llm.invoke(messages)
-
+    # print(len(response.tool_calls))
     return {
         "messages": [response],
-        "llm_calls": state.get("llm_calls", 0) + 1,
+        "llm_calls": state.get("llm_calls") + 1,
     }
 
 
-agent_builder = StateGraph(MessagesState)
-_ = agent_builder.add_node("llm_call", ingest_request)
-_ = agent_builder.add_edge(START, "llm_call")
-_ = agent_builder.add_edge("llm_call", END)
+graph = StateGraph(MessagesState)
+graph.add_node("call_model", call_model)
+graph.add_node("tools", ToolNode([write_request_response]))
+graph.add_edge(START, "call_model")
+graph.add_conditional_edges(
+    "call_model", tools_condition, {"tools": "tools", "__end__": "__end__"}
+)
+graph.add_edge("tools", "call_model")
+graph.add_edge("call_model", END)
 
-agent = agent_builder.compile()
+agent = graph.compile()
 
 
-result = agent.invoke({"messages": [HumanMessage(content="Hola")], "llm_calls": 0})
+# response = agent.invoke(
+#     {
+#         "messages": [HumanMessage(content="The form on the website is broken")],
+#         "llm_calls": 0,
+#     }
+# )
 
-for m in result["messages"]:
-    m.pretty_print()
+
+# messages = response["messages"]
+# for msg in messages:
+#     print(msg.pretty_print())
